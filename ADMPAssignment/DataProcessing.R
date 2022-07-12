a369b891c227a1fa9f3dafbce8229110e29ef079
Devi Priya <devipriya@Devis-MacBook-Air.local>
2022-07-12 09:41:09 +0100

    Updated
    Added crime category, type and area code

--- ADMPAssignment/DataProcessing.R
+++ ADMPAssignment/DataProcessing.R
@@ -1,6 +1,5 @@
 # prepareEnvr function: install and load multiple R packages.
 # check to see if packages are installed. Install them if they are not, then load them into the R session.
-
 prepareEnvr <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg)) 
@@ -14,27 +13,47 @@ prepareEnvr(packages)
 
 # Create variables for paths of the datasets
 path_crime <- "https://data.london.gov.uk/download/recorded_crime_summary/2bbd58c7-6be6-40ac-99ed-38c0ee411c8e/MPS%20Borough%20Level%20Crime%20%28Historical%29.csv"
-path_averagePrice <-"http://publicdata.landregistry.gov.uk/market-trend-data/house-price-index-data/Average-prices-2022-03.csv?utm_medium=GOV.UK&utm_source=datadownload&utm_campaign=average_price&utm_term=9.30_18_05_22"
+path_housePrice <-"http://publicdata.landregistry.gov.uk/market-trend-data/house-price-index-data/Average-prices-2022-03.csv?utm_medium=GOV.UK&utm_source=datadownload&utm_campaign=average_price&utm_term=9.30_18_05_22"
 path_salesVolume <- "http://publicdata.landregistry.gov.uk/market-trend-data/house-price-index-data/Sales-2022-03.csv?utm_medium=GOV.UK&utm_source=datadownload&utm_campaign=sales&utm_term=9.30_18_05_22"
+
+######################### Data Loading #####################################################
 
 # Load crime data from the csv file
 crime <- read.csv(path_crime,stringsAsFactors = TRUE, fileEncoding = "UTF-8")
+# Load House price dataset and Verify quick overview of data 
+housePrice <- read.csv( path_housePrice, stringsAsFactors = TRUE, fileEncoding = "UTF-8")
+# Load sales volume dataset and verify quick overview of data
+salesData <- read.csv(file = path_salesVolume, stringsAsFactors = TRUE, fileEncoding = "UTF-8")
+
+# Check the shape and quick summary of row data sets
+
+# Check crime data
 head(crime)
 skim(crime)
 cat('The shape of the data set is ', nrow(crime),'*', ncol(crime),'.')
 
+# Check average price data
+head(housePrice)
+skim(housePrice)
+cat('The shape of the data set is ', nrow(housePrice),'*', ncol(housePrice),'.')
+
+# Check the sales volume data
+head(salesData)
+skim(salesData)
+cat('The shape of the data set is ', nrow(salesData),'*', ncol(salesData),'.')
+
 
 ######################### Data Preprocessing ############################################### 
-
-# Removing unwanted from the YearMonth Column
+ ##### Crime Data #######
+# Removing unwanted from the YearMonth Column from Crime
 names(crime) = gsub(pattern = "*X", replacement="", x=names(crime)) 
 head(crime) 
 cat('The shape of the data set is ', nrow(crime),'*', ncol(crime),'.') # verify the shape of dataframe 
 
 # Check the Region_Name to identify the boroughs list for necessary transformation
 print(unique(crime$LookUp_BoroughName))
-# Replace "London Heathrow and London City Airports" with "City of London" to avoid data loss while merging
-# # Replace "Westminster" with "City of Westminster" to avoid data loss while merging with average price and sales volume dataset
+# Replace "London Heathrow and London City Airports" with "City of London" and "Westminster" with "City of Westminster"
+#       to avoid data loss while merging with average price and sales volume dataset
 crime$LookUp_BoroughName <- recode(crime$LookUp_BoroughName,
                   'London Heathrow and London City Airports' = 'City of London',
                   'Westminster'  = 'City of Westminster',
@@ -50,7 +69,8 @@ head(crime)
 # Renamed the columns headers as CrimeType, Region_Name, Date and NoOfCrime for MajorText, LookUp_BoroughName, variable and value.
 crime <- crime %>% 
   dplyr::rename(
-    CrimeType = MajorText,
+    CrimeType = MinorText,
+    CrimeCategory = MajorText,
     Region_Name = LookUp_BoroughName,
     Date = variable,
     NoOfCrime = value
@@ -61,33 +81,23 @@ head(crime)
 skim(crime)
 cat('The shape of the data set is ', nrow(crime),'*', ncol(crime),'.') 
 
-# Load House price dataset and Verify quick overview of data 
-averagePrice <- read.csv(file = path_averagePrice, stringsAsFactors = TRUE, fileEncoding = "UTF-8" )
-head(averagePrice)
-skim(averagePrice)
-cat('The shape of the data set is ', nrow(averagePrice),'*', ncol(averagePrice),'.')
-
+##### House Price Data ############
 # Replace the data column with month and date
-averagePrice$Date <- format(as.Date(averagePrice$Date,format = "%Y-%m-%d"),"%Y%m")
+housePrice$Date <- format(as.Date(housePrice$Date,format = "%Y-%m-%d"),"%Y%m")
 # Check first few rows to check the date with new format
-head(averagePrice) 
+head(housePrice) 
 # Check the shape
-cat('The shape of the average price is',dim(averagePrice)) 
+cat('The shape of the average price is',dim(housePrice)) 
 
-# Load sales volume dataset and verify quick overview of data
-salesData <- read.csv(file = path_salesVolume, stringsAsFactors = TRUE, fileEncoding = "UTF-8"  )
-head(salesData)
-skim(salesData)
-cat('The shape of the data set is ', nrow(salesData),'*', ncol(salesData),'.')
-
+##### Sales Data ##################
 # Replace the data column with month and date
 salesData$Date <- format(as.Date(salesData$Date,format = "%Y-%m-%d"),"%Y%m")
 head(salesData)
 cat('The shape of the sales volume is', dim(salesData))
 
-
+######################### Data Merge ############################################### 
 # Merge the 3 datasets by Date and borough
-df_list <- list(crime, averagePrice, salesData) 
+df_list <- list(crime, housePrice, salesData) 
 merged_data <- Reduce(function(x, y) merge(x, y, by=c("Region_Name","Date"), all=TRUE), df_list, accumulate=FALSE)
 head(merged_data, n = 10)
 glimpse(merged_data)
@@ -107,47 +117,48 @@ cat('The shape of the transformed data ', nrow(transformed_data),'*', ncol(trans
 ######################### Data cleaning and validation  ####################################
 
 # Remove data with selected time period
-data_cleaned<- subset(transformed_data, Date %in% 201601:202012)
-data_cleaned[
-  order( data_cleaned[,2], data_cleaned[,1] ),
-]
+data_cleaned <- subset(transformed_data, Date %in% 201601:202012)
+# data_cleaned <- data_cleaned[with(data_cleaned, order(Date, Region_Name)), ]
+data_cleaned <- arrange(data_cleaned,Date,Region_Name)
 head(data_cleaned)
-cat('The shape of the data within the time period ', nrow(data_cleaned),'*', ncol(data_cleaned),'.') # checking shape
+# Check shape
+cat('The shape of the data within the time period ', nrow(data_cleaned),'*', ncol(data_cleaned),'.') 
 cat('Removed row',(nrow(transformed_data)-nrow(data_cleaned)),'\nRemoved columns',(ncol(transformed_data)-ncol(data_cleaned)))
 head(data_cleaned)
 
 # Remove unwanted columns
-data_cleaned[ , c('MinorText','Area_Code.x','Area_Code.y','Monthly_Change','Annual_Change','Average_Price_SA')] <- list(NULL)
+data_cleaned[ , c('Area_Code.y','Monthly_Change','Annual_Change','Average_Price_SA')] <- list(NULL)
+data_cleaned$Area_Code <- data_cleaned$Area_Code.x
 head(data_cleaned)
 cat('The shape of the data after dropping unwanted columns', nrow(data_cleaned),'*', ncol(data_cleaned),'.') # checking the shape
 
 # Group rows with same type of crime, date and borough by taking total of crimes.
-data_cleaned <- aggregate( cbind( NoOfCrime ) ~ Region_Name + Date + CrimeType + Average_Price + Sales_Volume + revenue, 
+data_cleaned <- aggregate( cbind( NoOfCrime ) ~ Region_Name + Area_Code + Date + CrimeCategory + CrimeType + Average_Price + Sales_Volume + revenue,
                     data = data_cleaned , FUN = sum)
 head(data_cleaned)
 cat('The shape of the data after aggregation ', nrow(data_cleaned),'*', ncol(data_cleaned),'.') # checking the shape
 glimpse(data_cleaned)
 skim(data_cleaned)
 
+# Clean the column names
+data_cleaned<-clean_names(data_cleaned)
+head((data_cleaned))
+
 # Remove unwanted observations where number of crimes are 0.
-data_cleaned<-data_cleaned[data_cleaned$NoOfCrime != 0, ]
+data_cleaned<-data_cleaned[data_cleaned$no_of_crime != 0, ]
 head(data_cleaned)
 cat('The shape of the dataframe is', dim(data_cleaned))
-
 
 # Validate missing values in the dataset
 any(is.na(data_cleaned))
 cat("The total number of null values in the dataset ", sum(is.na(data_cleaned)))
 
+
 # Remove missing values from the dataset
-data_cleaned <- data_cleaned %>% filter(!is.na(final))
+data_cleaned <- data_cleaned %>% drop_na()
 cat('The shape of the dataframe is', dim(data_cleaned))
 glimpse(data_cleaned)
 skim(data_cleaned)
-
-# Clean the column names
-data_cleaned<-clean_names(data_cleaned)
-head((data_cleaned))
 
 # Validate rows with missing values
 rownames(data_cleaned)[apply(data_cleaned, 2, anyNA)]
@@ -157,11 +168,19 @@ cat('The shape of the dataframe is', dim(data_cleaned))
 data_cleaned <- remove_empty(data_cleaned, which = c("rows","cols"), quiet = FALSE)
 cat('The shape of the dataframe is', dim(data_cleaned))
 
+# Validate the duplicates in the data
+cat('The number of duplicate entries in the data are', sum(duplicated(data_cleaned)))
+
 # Remove duplicates
-data_cleaned %>% distinct()
+data_cleaned <- data_cleaned %>% distinct()
+head(data_cleaned)
+cat('The shape of the dataframe is', dim(data_cleaned))
+
+#Sort the data with boroughname and date 
+data_cleaned <- data_cleaned[with(data_cleaned, order(date,region_name)),]
 head(data_cleaned)
 cat('The shape of the dataframe is', dim(data_cleaned))
 
 
 # Write the dataset to single csv file to check the merged dataset
-write.csv(data_cleaned, file = "data.csv")
+write.csv(data_cleaned, file = "data.csv", row.names = FALSE)
